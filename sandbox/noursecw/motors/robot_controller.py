26d4767e506aa52bc19c3c1a7ab49bf1a3fa561d
noursecw <noursecw@rose-hulman.edu>
2018-01-31 16:54:23 -0500

    m3 complete

--- sandbox/noursecw/motors/robot_controller.py
+++ sandbox/noursecw/motors/robot_controller.py
@@ -18,7 +18,7 @@ import time
 
 class Snatch3r(object):
     """Commands for the Snatch3r robot that might be useful in many different programs."""
-    
+
     # TODO: Implement the Snatch3r class as needed when working the sandox exercises
     # (and delete these comments)
     def __init__(self):
@@ -46,4 +46,4 @@ class Snatch3r(object):
 
         self.right_motor.wait_while(ev3.Motor.STATE_RUNNING)
         self.left_motor.wait_while(ev3.Motor.STATE_RUNNING)
-        # ev3.Sound.beep().wait()
+        ev3.Sound.beep().wait()
