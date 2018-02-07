243ad4399cfed8984a77f8376e506d3ae24805b8
noursecw <noursecw@rose-hulman.edu>
2018-02-07 17:24:55 -0500

    m1: color sensors - TODOS 1, 2, 3 (not working)

--- libs/robot_controller.py
+++ libs/robot_controller.py
@@ -27,7 +27,9 @@ class Snatch3r(object):
         self.arm_motor = ev3.MediumMotor(ev3.OUTPUT_A)
         self.touch_sensor = ev3.TouchSensor(ev3.INPUT_1)
         self.running = False
+        self.color_sensor = ev3.ColorSensor()
 
+        assert self.color_sensor
         assert self.left_motor.connected
         assert self.right_motor.connected
         assert self.arm_motor
