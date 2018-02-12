4878331e73de0c321134e3b06d01671830355287
ricet <ricet@rose-hulman.edu>
2018-02-11 21:18:11 -0500

    finished m1 beep at blue, m5 pc remote drive

--- sandbox/noursecw/analog_sensors/pixy/m1_beep_at_blue.py
+++ sandbox/noursecw/analog_sensors/pixy/m1_beep_at_blue.py
@@ -35,6 +35,15 @@ def main():
         #   self.pixy = ev3.Sensor(driver_name="pixy-lego")
         #   assert self.pixy
         # Then here you can use a command like width = robot.pixy.value(3)
+        print("(X, Y) = ({}, {})    Width = {} Height = {}".format(
+            robot.pixy.value(1), robot.pixy.value(2), robot.pixy.value(3),
+            robot.pixy.value(
+                4)))
+
+        if robot.pixy.value(2) > 0:
+            ev3.Sound.beep()
+
+        time.sleep(0.5)
 
 
 
