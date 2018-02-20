a2499d7362682f3b1ac7cf665b2f1114dc7558af
ricet <ricet@rose-hulman.edu>
2018-02-20 02:18:37 -0500

    Working on ui

--- projects/ricet/ricet_pc.py
+++ projects/ricet/ricet_pc.py
@@ -10,18 +10,28 @@ import mqtt_remote_method_calls as com
 
 
 # class EventHandler(object):
+class UIhandler(object):
+    def new_commands(self, event):
+        print('new commands')
 
 
 def main():
+    handler = UIhandler()
     root = tkinter.Tk()
     root.title = "ricet pc"
 
-    main_frame = ttk.Frame(root, padding=5)
+    main_frame = ttk.Frame(root, padding=5, borderwidth=10)
     main_frame.grid()
 
-    canvas = tkinter.Canvas(main_frame, background="lightgray", width=800,
-                            height=500)
-    canvas.grid(columnspan=2)
+    instructions = ttk.Label(main_frame,
+                             text="Try to get as close to the beacon "
+                                  "as possible!")
+    instructions.grid(pady=10)
+
+    button = ttk.Button(main_frame, padding=4, text='New commands')
+    button.bind("<Button-1>", lambda event: handler.new_commands(event))
+    button.grid(sticky='W')
+    
 
     root.mainloop()
 
