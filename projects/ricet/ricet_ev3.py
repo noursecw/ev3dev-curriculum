76ee9727d059736b9892d63fedb6552dba47596c
ricet <ricet@rose-hulman.edu>
2018-02-20 18:48:45 -0500

    Doc strings, fixed whack dropdown

--- projects/ricet/ricet_ev3.py
+++ projects/ricet/ricet_ev3.py
@@ -1,12 +1,10 @@
+"""
+Robot code for csse120 project.
+Author: Tiarnan Rice
+"""
+
 import mqtt_remote_method_calls as com
 import robot_controller as robo
-
-
-class DataContainer(object):
-    """ Helper class that might be useful to communicate between different callbacks."""
-
-    def __init__(self):
-        self.running = True
 
 
 def main():
@@ -15,29 +13,32 @@ def main():
     print("/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/")
 
     robot = robo.Snatch3r()
-    dc = DataContainer()
     command_handler = CommandHandler(robot)
     mqtt_client = com.MqttClient(command_handler)
     command_handler.mqtt_client = mqtt_client
     mqtt_client.connect_to_pc()
 
-    while not robot.touch_sensor.is_pressed and dc.running:
-        print('running')
+    while not robot.touch_sensor.is_pressed:
+        print('', end='')
 
     print("Goodbye!")
 
 
 class CommandHandler:
+    """Handles incoming mqtt commands."""
     def __init__(self, robot):
         self.robot = robot
         self.commands = []
         self.mqtt_client = None
 
     def receive_commands(self, commands):
+        """Creates new list to store incoming commands"""
         self.commands = commands
         print(self.commands)
 
     def run_commands(self):
+        """Runs stored commands, checking which command it is then calling
+        the appropriate function in robot_controller"""
         beacon = False
         for i in range(len(self.commands)):
             if self.commands[i] == 'forward_10':
@@ -62,16 +63,19 @@ class CommandHandler:
                 print('Not a command')
 
         while not beacon:
+            print('searching')
             x = self.robot.pixy.value(1)
             if x < 150:
                 self.robot.turn_left(100, 100)
             elif x > 170:
                 self.robot.turn_right(100, 100)
-            elif 150 <= x <= 170:
+            elif x >= 150 and x <= 170:
                 self.robot.stop()
                 beacon = True
 
-        distance = self.robot.pixy.value(3) * 1
+        distance = (self.robot.pixy.value(3) ** 2) * 0.06 + \
+                   self.robot.pixy.value(3) * 4.42 - 3.17
+        print(distance)
 
         self.mqtt_client.send_message('on_distance_receive', [distance])
 
--- projects/ricet/ricet_pc.py
+++ projects/ricet/ricet_pc.py
@@ -1,5 +1,5 @@
 """
-
+User interface code for csse120 project.
 Author: Tiarnan Rice
 """
 
@@ -13,6 +13,8 @@ import mqtt_remote_method_calls as com
 
 
 class UIhandler(object):
+    """Handles all the tkinter button presses, as well as the mqtt
+    callbacks."""
     def __init__(self):
         self.commands = []
         self.commandlist = ['forward_10', 'forward_20',
@@ -22,6 +24,8 @@ class UIhandler(object):
         self.new_commands(event=None, uicommands=None)
 
     def new_commands(self, event, uicommands):
+        """New commands updates commands[] with a new set from the
+    commandlist randomly."""
         print('new commands')
         self.commands = []
         for _ in range(5):
@@ -32,12 +36,16 @@ class UIhandler(object):
         if uicommands is not None:
             uicommands.set_commands()
 
-    def on_distance_recieve(self, distance):
+    def on_distance_receive(self, distance):
+        """on_distance_receive is called when an mqtt
+    message containing the distance is received. Creates a popup window with the value."""
         print("Made it to ", distance, " inches away")
         messagebox.showinfo(title='Congratulations!', message='You made '
                                                               'it to %d inches away!' % distance)
 
     def send_commands(self, event, mqtt_client, uicommands):
+        """   send_commands sends the
+    current commands, in the right order, to the robot. run"""
         print("Sending commands")
         send_commands = []
         for i in range(len(uicommands.commandVals)):
@@ -47,10 +55,12 @@ class UIhandler(object):
         mqtt_client.send_message('receive_commands', [send_commands])
 
     def run_commands(self, event, mqtt_client):
+        """Tells the robot to run the stored commands."""
         print("Running commands")
         mqtt_client.send_message('run_commands', [])
 
     def quit(self, event, mqtt_client, root):
+        """Shuts down the client"""
         print('Quitting')
         root.quit()
 
@@ -61,17 +71,20 @@ def main():
     mqtt_client.connect_to_ev3()
 
     root = tkinter.Tk()
-    root.title = "ricet pc"
+    root.title('Ricet Pc')
 
     main_frame = ttk.Frame(root, padding=5, borderwidth=10)
     main_frame.grid()
 
-    uicommands = UIcommands(handler, main_frame)
+    command_frame = ttk.Frame(main_frame, padding=5)
+    command_frame.grid(row=3, column=0)
+
+    uicommands = UIcommands(handler, command_frame)
 
     instructions = ttk.Label(main_frame,
                              text="Try to get as close to the beacon "
                                   "as possible!")
-    instructions.grid(pady=10, row=1)
+    instructions.grid(pady=10, row=1, column=0)
 
     newcommands = ttk.Button(main_frame, padding=4, text='New commands')
     newcommands.bind("<Button-1>", lambda event: handler.new_commands(event,
@@ -97,64 +110,70 @@ def main():
 
 
 class UIcommands():
+    """Separate class for command drop downs. This is necessary to be able
+    to change the contents of the drop downs on the fly. Init creates
+    initial drop downs, set_commands updates them with new commands."""
     def __init__(self, handler, frame):
+        """Creates initial OptionMenus, gets values from handler"""
         self.frame = frame
         self.handler = handler
         self.c1 = tkinter.StringVar(frame)
         self.c1.set(handler.commands[0])
         self.command1 = tkinter.OptionMenu(frame, self.c1, *handler.commands)
-        self.command1.grid(row=3, pady=10, column=0)
+        self.command1.grid(row=3, pady=10, column=1)
 
         self.c2 = tkinter.StringVar(frame)
         self.c2.set(handler.commands[1])
         self.command2 = tkinter.OptionMenu(frame, self.c2, *handler.commands)
-        self.command2.grid(row=3, pady=10, column=1)
+        self.command2.grid(row=3, pady=10, column=2)
 
         self.c3 = tkinter.StringVar(frame)
         self.c3.set(handler.commands[2])
         self.command3 = tkinter.OptionMenu(frame, self.c3, *handler.commands)
-        self.command3.grid(row=3, pady=10, column=2)
+        self.command3.grid(row=3, pady=10, column=3)
 
         self.c4 = tkinter.StringVar(frame)
         self.c4.set(handler.commands[3])
         self.command4 = tkinter.OptionMenu(frame, self.c4, *handler.commands)
-        self.command4.grid(row=3, pady=10, column=3)
+        self.command4.grid(row=3, pady=10, column=4)
 
         self.c5 = tkinter.StringVar(frame)
         self.c5.set(handler.commands[4])
         self.command5 = tkinter.OptionMenu(frame, self.c5, *handler.commands)
-        self.command5.grid(row=3, pady=10, column=4)
+        self.command5.grid(row=3, pady=10, column=5)
 
         self.commands = [self.command1, self.command2, self.command3,
                          self.command4, self.command5]
         self.commandVals = [self.c1, self.c2, self.c3, self.c4, self.c5]
 
+    #   remakes all the command dropdowns with new options
     def set_commands(self):
+        """Recreates drop-downs with new options."""
         print('set commands ui')
         self.c1.set(self.handler.commands[0])
         self.command1 = tkinter.OptionMenu(self.frame, self.c1,
                                            *self.handler.commands)
-        self.command1.grid(row=3, pady=10, column=0)
+        self.command1.grid(row=3, pady=10, column=1)
 
         self.c2.set(self.handler.commands[1])
         self.command2 = tkinter.OptionMenu(self.frame, self.c2,
                                            *self.handler.commands)
-        self.command2.grid(row=3, pady=10, column=1)
+        self.command2.grid(row=3, pady=10, column=2)
 
         self.c3.set(self.handler.commands[2])
         self.command3 = tkinter.OptionMenu(self.frame, self.c3,
                                            *self.handler.commands)
-        self.command3.grid(row=3, pady=10, column=2)
+        self.command3.grid(row=3, pady=10, column=3)
 
         self.c4.set(self.handler.commands[3])
         self.command4 = tkinter.OptionMenu(self.frame, self.c4,
                                            *self.handler.commands)
-        self.command4.grid(row=3, pady=10, column=3)
+        self.command4.grid(row=3, pady=10, column=4)
 
         self.c5.set(self.handler.commands[4])
         self.command5 = tkinter.OptionMenu(self.frame, self.c5,
                                            *self.handler.commands)
-        self.command5.grid(row=3, pady=10, column=4)
+        self.command5.grid(row=3, pady=10, column=5)
 
         self.commands = [self.command1, self.command2, self.command3,
                          self.command4, self.command5]
